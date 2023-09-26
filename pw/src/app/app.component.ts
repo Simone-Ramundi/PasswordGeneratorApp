46bd08de036b108681b3b7c1fe863e09511460b5
Simone-Ramundi <simoneramundi@outlook.com>
2023-09-26 17:43:15 +0200

    Second commit

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  password = '';
   onButtonClick() {
-    console.log('Button was clicked!')
+    this.password= 'My password';
   }
 }

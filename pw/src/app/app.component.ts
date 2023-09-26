0585e3e51f214cfe598ae6cc7371e0f51401f4c4
Simone-Ramundi <simoneramundi@outlook.com>
2023-09-26 16:44:43 +0200

    First commit

---
+++ pw/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  onButtonClick() {
+    console.log('Button was clicked!')
+  }
+}

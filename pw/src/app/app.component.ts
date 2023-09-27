8776d1f1d5a33f7c645deb919c5ea79bacfa2d7f
Simone-Ramundi <simoneramundi@outlook.com>
2023-09-27 16:40:35 +0200

    Project finished - Generating a Random Password

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,8 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length: number = 0;
+  includeLetters=false;
+  includeNumbers=false;
+  includeSymbols=false;
   password = '';
   onButtonClick() {
-    this.password= 'My password';
+    const numbers = '1234567890';
+    const letters = 'abcdefghilmnopqrstuvz';
+    const symbols = '!£$%&';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters
+    }
+    if(this.includeNumbers){
+      validChars += numbers
+    }
+    if(this.includeSymbols){
+      validChars += symbols
+    }
+
+    let generatedPassword = '';
+    for (let i=0;i < this.length; i++){
+    const index = Math.floor(Math.random()*validChars.length)
+      generatedPassword += validChars[index]
+    }
+    this.password = generatedPassword;
+  }
+    onChangeLength(value:string){
+     const parsedValue= parseInt(value);
+     if(!isNaN(parsedValue)){
+       this.length = parsedValue;
+     }
+    }
+    onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters
+  }
+
+    onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers
+  }
+
+    onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols
   }
 }
